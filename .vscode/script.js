669777ec325c6376a1dfef067721760cdc4db338
Prabhdeep0101 <prabhdeep24823@gmail.com>
2025-04-14 10:41:20 -0700

    portfolio.html

--- .vscode/script.js
+++ .vscode/script.js
@@ -1,3 +1,20 @@
+<<<<<<< HEAD
+function showAlert() {
+    alert('Hello! Welcome to my personal website.');
+}
+function showDate() {
+    document.getElementById('dateDisplay').innerText = new Date().toDateString();
+}
+function updateClock() {
+    const now = new Date();
+    const timeString = now.toLocaleTimeString();
+    document.getElementById('clock').innerText = timeString;
+}
+
+setInterval(updateClock, 1000); // Update every second
+updateClock(); // Initial call to display immediately
+
+=======
 function showAlert() {
     alert('Hello! Welcome to MY PERSONAL WEBSITE.');
 }
@@ -13,4 +30,5 @@ function updateClock() {
 setInterval(updateClock, 1000); 
 updateClock(); 
 
+>>>>>>> f3740da (Add portfolio.html with class projects and skills)
  
